0110f83642c7d8b560b9dd188faea8f15baffd1b
alsciende <bertolini.cedric@me.com>
2016-01-22 11:40:05 +0100

    new command to delete a decklist

---
+++ src/AppBundle/Command/DeleteDecklistCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+class DeleteDecklistCommand extends ContainerAwareCommand
+{
+
+	protected function configure()
+	{
+		$this
+		->setName('app:decklist:delete')
+		->setDescription('Delete one decklist')
+        ->addArgument(
+            'decklist_id',
+            InputArgument::REQUIRED,
+            'Id of the decklist'
+        )
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$em = $this->getContainer()->get('doctrine')->getManager();
+		
+		$decklist_id = $input->getArgument('decklist_id');
+		$decklist = $em->getRepository('AppBundle:Decklist')->find($decklist_id);
+		
+		$successors = $em->getRepository('AppBundle:Decklist')->findBy(array(
+				'precedent' => $decklist
+		));
+		foreach($successors as $successor) {
+			/* @var $successor \AppBundle\Entity\Decklist */
+			$successor->setPrecedent(null);
+		}
+		
+		$children = $em->getRepository('AppBundle:Deck')->findBy(array(
+				'parent' => $decklist
+		));
+		foreach($children as $child) {
+			/* @var $child \AppBundle\Entity\Deck */
+			$child->setParent(null);
+		}
+		
+		$em->flush();
+		$em->remove($decklist);
+		$em->flush();
+		
+		$output->writeln("Decklist deleted");
+	}
+}
