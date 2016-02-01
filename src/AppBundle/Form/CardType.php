d2d709ac18b6fa045a27ca0c6a0af7a40ff0c526
alsciende <bertolini.cedric@me.com>
2016-02-01 09:44:20 +0100

    add octgn id to card form

--- src/AppBundle/Form/CardType.php
+++ src/AppBundle/Form/CardType.php
@@ -29,6 +29,7 @@ class CardType extends AbstractType
             ->add('traits')
             ->add('flavor', 'textarea', array('required' => false))
             ->add('illustrator')
+            ->add('octgnid')
             ->add('is_unique', 'checkbox', array('required' => false))
             ->add('is_loyal', 'checkbox', array('required' => false))
             ->add('is_military', 'checkbox', array('required' => false))
