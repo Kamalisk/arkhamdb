7ba100d5fb22ffbd58927a280d2c5ad9b3a3dda0
alsciende <bertolini.cedric@me.com>
2016-03-24 15:31:11 +0100

    ui tweaks: navbar and tooltips

--- src/AppBundle/Resources/public/js/app.tip.js
+++ src/AppBundle/Resources/public/js/app.tip.js
@@ -13,7 +13,7 @@ function display_card_on_element(card, element, event) {
 		+ '<h4 class="card-name">' + app.format.name(card) + '</h4>'
 		+ '<div class="card-info">' + app.format.info(card) + '</div>'
 		+ '<div class="card-traits">' + app.format.traits(card) + '</div>'
-		+ '<div class="card-text">' + app.format.text(card) + '</div>'
+		+ '<div class="card-text border-'+card.faction_code+'">' + app.format.text(card) + '</div>'
 		+ '<span class="card-pack pull-right" style="clear:right">' + app.format.pack(card) + '</span>'
 		+ '<span class="card-faction">' + app.format.faction(card) + '</span>';
 	}
