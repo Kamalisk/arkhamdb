23862b468374ae7a2a26319942a597830dc11d3f
Allan Clements <w.github@kamalisk.com>
2017-03-01 22:47:37 +0000

    set to utf8

--- src/AppBundle/Resources/public/js/app.deck_history.js
+++ src/AppBundle/Resources/public/js/app.deck_history.js
@@ -131,7 +131,7 @@ deck_history.all_changes = function all_changes() {
 	_.each(cards_exiled, function (qty, code) {
 		var card = app.data.cards.findById(code);
 		if(!card) return;
-		exile_list.push('&minus;'+qty+' '+'<a href="'+card.url+'" class="card card-tip fg-'+card.faction_code+'" data-toggle="modal" data-remote="false" data-target="#cardModal" data-code="'+card.code+'">'+card.name+'</a>'+(card.xp >= 0 ? "•".repeat(card.xp) : '')+'</a>');
+		exile_list.push('&minus;'+qty+' '+'<a href="'+card.url+'" class="card card-tip fg-'+card.faction_code+'" data-toggle="modal" data-remote="false" data-target="#cardModal" data-code="'+card.code+'">'+card.name+'</a>'+(card.xp >= 0 ? "â€¢".repeat(card.xp) : '')+'</a>');
 		//remove_list.push('&minus;'+qty+' '+'<a href="'+Routing.generate('cards_zoom',{card_code:code})+'" class="card-tip" data-code="'+code+'">'+card.name+'</a>');
 	});
 	if (cost && app.deck.get_previous_deck()){
