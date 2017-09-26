486bf5c22e793142b4a8a42d60ee74ef9f84ea46
Allan Clements <w.github@kamalisk.com>
2017-09-26 12:06:29 +0100

    added more letters in card codes added faqs text

--- src/AppBundle/Services/CardsData.php
+++ src/AppBundle/Services/CardsData.php
@@ -317,7 +317,7 @@ class CardsData
 						{
 							$or = [];
 							foreach($condition as $arg) {
-								$code = preg_match('/^\d\d(\d\d\d[ab]?|_[a-zA-Z0-9]+)$/u', $arg);
+								$code = preg_match('/^\d\d(\d\d\d[abcdefgh]?|_[a-zA-Z0-9]+)$/u', $arg);
 								$acronym = false;////preg_match('/^[A-Z]{2,}$/', $arg);
 								if($code) {
 									$or[] = "(c.code = ?$i)";
