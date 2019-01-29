b6abd4f88e14bcde7da1df280ae83d78f0fbc2cf
Allan Clements <w.github@kamalisk.com>
2019-01-29 22:45:28 +0000

    default xp_adjustment to 0 to help avoid caching issues

--- src/AppBundle/Controller/BuilderController.php
+++ src/AppBundle/Controller/BuilderController.php
@@ -561,8 +561,8 @@ class BuilderController extends Controller
 		$name = filter_var($request->get('name'), FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		$problem = filter_var($request->get('problem'), FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		$decklist_id = filter_var($request->get('decklist_id'), FILTER_SANITIZE_NUMBER_INT);
-		$xp_spent = filter_var($request->get('xp_spent'), FILTER_SANITIZE_NUMBER_INT);
-		$xp_adjustment = filter_var($request->get('xp_adjustment'), FILTER_SANITIZE_NUMBER_INT);
+		$xp_spent = filter_var($request->get('xp_spent', 0), FILTER_SANITIZE_NUMBER_INT);
+		$xp_adjustment = filter_var($request->get('xp_adjustment', 0), FILTER_SANITIZE_NUMBER_INT);
 		$description = trim($request->get('description'));
 		$tags = filter_var($request->get('tags'), FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
