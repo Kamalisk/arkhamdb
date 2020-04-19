20294ead8588bb3f1c01b61c061e825ab1c3d5bf
Allan Clements <w.github@kamalisk.com>
2020-04-19 18:58:41 +0000

    add sorting to investigator select

--- src/AppBundle/Resources/public/js/app.deckinit.js
+++ src/AppBundle/Resources/public/js/app.deckinit.js
@@ -4,6 +4,7 @@
 
 	var faction_selected = "";
 	var owned = false;
+	var order = "name";
 
 	deck_init.show_faction = function(faction) {
 		deck_init.faction_selected = faction;
@@ -13,12 +14,57 @@
 		deck_init.owned = !deck_init.owned;
 		deck_init.update_display();
 		deck_init_config.all = !deck_init.owned;
-		console.log(deck_init_config, deck_init.owned);
 		if (localStorage) {
 			localStorage.setItem('ui.deck.init', JSON.stringify(deck_init_config));
 		}
 	}
+	deck_init.change_order = function() {
+		deck_init.order = $("#order").val();
+		deck_init_config.order = deck_init.order;
+		if (localStorage) {
+			localStorage.setItem('ui.deck.init', JSON.stringify(deck_init_config));
+		}
+		deck_init.update_display();
+	}
+	deck_init.sort = function (order) {
+		var divs = $("#investigator_divs > div");
+
+		var sort_name_function = function (a, b) {
+			if ($(a).attr("data-name").replace(/[^A-Za-z0-9 ]/, "") > $(b).attr("data-name").replace(/[^A-Za-z0-9 ]/, "")) {
+				return 1;
+			}
+			return -1;
+		}
+
+		var sort_function = sort_name_function;
+		if (order == "faction") {
+			sort_function = function(a, b) {
+				if ($(a).attr("data-class") == $(b).attr("data-class")) {
+					return sort_name_function(a, b);
+				}
+				if ($(a).attr("data-class") > $(b).attr("data-class")) {
+					return 1;
+				}
+				return -1;
+			}
+		} else if (order == "willpower" || order == "agility" || order == "combat" || order == "intellect") {
+			sort_function = function(a, b) {
+				if ($(a).attr("data-"+order) == $(b).attr("data-"+order)) {
+					return sort_name_function(a, b);
+				}
+				if ($(a).attr("data-"+order) > $(b).attr("data-"+order)) {
+					return -1;
+				}
+				return 1;
+			}
+		}
+
+		divs.sort(sort_function);
+		$("#investigator_divs").empty();
+		$("#investigator_divs").append(divs);
+	}
 	deck_init.update_display = function () {
+		deck_init.sort(deck_init.order);
 		$(".faction-filter").removeClass("selected");
 		if (deck_init.faction_selected) {
 			$("#filter_title").text(deck_init.faction_selected.charAt(0).toUpperCase() + deck_init.faction_selected.substring(1));
@@ -53,7 +99,7 @@
 			localStorage.setItem('ui.deck.init', JSON.stringify(deck_init_config));
 		}
 	}
-	
+
 	$(document).ready(function(){
 		if (localStorage) {
 			var stored = localStorage.getItem('ui.deck.init');
@@ -64,11 +110,15 @@
 		deck_init_config = _.extend({
 			'all': true,
 		}, deck_init_config || {});
-		
+
 		if (deck_init_config.all){
 			deck_init.owned = false;
 		} else {
 			deck_init.owned = true;
+		}
+		if (deck_init_config.order){
+			deck_init.order = deck_init_config.order;
+			$("#order").val(deck_init.order);
 		}
 		$("#deck_init_all").attr("checked", deck_init.owned);
 		deck_init.show_faction('');
