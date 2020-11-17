a1cfa4127ff69ac5f5b8f4032da16cf38b8a30b8
Allan Clements <w.github@kamalisk.com>
2020-11-17 00:37:36 +0000

    temp fix for unescapedness

--- src/AppBundle/Resources/public/js/app.markdown.js
+++ src/AppBundle/Resources/public/js/app.markdown.js
@@ -3,19 +3,19 @@
 	markdown.setup = function setup(textarea, preview) 
 	{
 		$(textarea).on('change keyup', function() {
-			$(preview).html(marked($(textarea).val()))
+			$(preview).text(marked($(textarea).val()))
 		});
 		$(textarea).trigger('change');
 	}
 
 	markdown.refresh = function refresh(textarea, preview) 
 	{
-		$(preview).html(marked($(textarea).val()))
+		$(preview).text(marked($(textarea).val()))
 	}
 
 	markdown.update = function update(text_markdown, preview) 
 	{
-		$(preview).html(marked(text_markdown))
+		$(preview).text(marked(text_markdown))
 	}
 
 })(app.markdown = {}, jQuery);
