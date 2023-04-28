7cc305a34a4b3a51a019aea183c69ca0e971a08c
Daniel Salinas <daniel@Daniels-MacBook-Pro.local>
2023-04-28 10:25:09 -0400

    Ancestral Knowledge shouldn't count Vincent/Amanda sigs
    Amandas is a Weakness and Vincents isn't part of his deck.
    Someday this might break if we get a Seeker that can take it, but we can revise then

--- src/AppBundle/Resources/public/js/app.deck.js
+++ src/AppBundle/Resources/public/js/app.deck.js
@@ -1499,7 +1499,7 @@ deck.reset_limit_count = function (){
 		}
 		if (ancestral_knowledge && ancestral_knowledge.indeck) {
 			// We put it at the front of the requirements since it needs to count things.
-			var new_option = {name: "ancestral_knowledge", dynamic: true, type:["skill"], atleast: { types: 1, min: 10 }, atleast_count: {}, ignore_match: true };
+			var new_option = {name: "ancestral_knowledge", dynamic: true, type:["skill"], level: { min: 0, max: 5}, atleast: { types: 1, min: 10 }, atleast_count: {}, ignore_match: true };
 			deck.deck_options.unshift(new_option);
 		}
 		if (versatile && versatile.indeck) {
