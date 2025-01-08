7f54641152da9dc1cb569f9f67e5cd515e8f83fc
Allan Clements <w.github@kamalisk.com>
2025-01-08 16:55:37 +0000

    fix password reset page

--- src/AppBundle/Controller/ResettingController.php
+++ src/AppBundle/Controller/ResettingController.php
@@ -36,16 +36,16 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ResettingController extends BaseController
 {
-    private $eventDispatcher;
-    private $formFactory;
-    private $userManager;
-    private $tokenGenerator;
-    private $mailer;
+    protected $eventDispatcher;
+    protected $formFactory;
+    protected $userManager;
+    protected $tokenGenerator;
+    protected $mailer;
 
     /**
      * @var int
      */
-    private $retryTtl;
+    protected $retryTtl;
 
     /**
      * @param EventDispatcherInterface $eventDispatcher
@@ -151,5 +151,58 @@ class ResettingController extends BaseController
         }
 
         return new RedirectResponse($this->generateUrl('fos_user_resetting_check_email', array('username' => $username)));
+    }
+
+    /**
+     * Reset user password.
+     *
+     * @param Request $request
+     * @param string  $token
+     *
+     * @return Response
+     */
+    public function resetAction(Request $request, $token)
+    {
+        $user = $this->userManager->findUserByConfirmationToken($token);
+
+        if (null === $user) {
+            return new RedirectResponse($this->container->get('router')->generate('fos_user_security_login'));
+        }
+
+        $event = new GetResponseUserEvent($user, $request);
+        $this->eventDispatcher->dispatch(FOSUserEvents::RESETTING_RESET_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $this->formFactory->createForm();
+        $form->setData($user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $event = new FormEvent($form, $request);
+            $this->eventDispatcher->dispatch(FOSUserEvents::RESETTING_RESET_SUCCESS, $event);
+
+            $this->userManager->updateUser($user);
+
+            if (null === $response = $event->getResponse()) {
+                $url = $this->generateUrl('fos_user_profile_show');
+                $response = new RedirectResponse($url);
+            }
+
+            $this->eventDispatcher->dispatch(
+                FOSUserEvents::RESETTING_RESET_COMPLETED,
+                new FilterUserResponseEvent($user, $request, $response)
+            );
+
+            return $response;
+        }
+
+        return $this->render('@FOSUser/Resetting/reset.html.twig', array(
+            'token' => $token,
+            'form' => $form->createView(),
+        ));
     }
 }
